415d47e86af0271073a6699c3d9018c77f5fd115
meszon <r.meszon@gmail.com>
2020-10-30 17:07:38 +0100

    started

--- src/part1/MailStore.java
+++ src/part1/MailStore.java
@@ -1,4 +1,16 @@
 package part1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailStore {
+    List<Message> messageList = new ArrayList<>();
+
+    private void sendNewMessage (){
+
+    }
+
+    private void retrieveAllMessages() {
+
+    }
 }
--- src/part1/Message.java
+++ src/part1/Message.java
@@ -1,4 +1,10 @@
 package part1;
 
+import java.util.Date;
+
 public class Message {
+    String subject;
+    String sender;
+    String receiver;
+    Date messageCreation;
 }
