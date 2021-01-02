a883d1fbae9a7efff58fffaff0913a76f45b25f1
Raül Mesa <raul.mesa@estudiants.urv.cat>
2021-01-02 18:03:20 +0100

    javadoc - User

--- src/part1/User.java
+++ src/part1/User.java
@@ -5,20 +5,38 @@ public class User {
     private final String name;
     private final int yearBirth;
 
+    /**
+     * Constructor
+     * @param username -> username
+     * @param name -> name of user
+     * @param yearBirth -> birth year
+     */
     public User(String username, String name, int yearBirth) {
         this.username = username;
         this.name = name;
         this.yearBirth = yearBirth;
     }
 
+    /**
+     * Returns username from User
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns name from User
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns birth year from User
+     * @return birth year
+     */
     public int getYearBirth() {
         return yearBirth;
     }
