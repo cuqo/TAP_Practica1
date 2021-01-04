31f01f13742f7fae06631589669807463ceb56dc
Raül Mesa <raul.mesa@estudiants.urv.cat>
2021-01-04 17:23:33 +0100

    junit part1

--- src/part2/junitTests/UnitTestRunner.java
+++ src/part2/junitTests/UnitTestRunner.java
@@ -1,8 +1,10 @@
-package part1.junitTests;
+package part2.junitTests;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import part1.junitTests.MailSystemUnitTest;
+import part1.junitTests.MailboxUnitTest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +13,7 @@ public class UnitTestRunner {
 
     public static void main(String[] args) {
         List<Result> results = new ArrayList<>();
-        results.add(JUnitCore.runClasses(MailStoreUnitTest.class));
+        //results.add(JUnitCore.runClasses(MailStoreUnitTest.class));
         results.add(JUnitCore.runClasses(MailboxUnitTest.class));
         results.add(JUnitCore.runClasses(MailSystemUnitTest.class));
 
